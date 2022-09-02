50619c1e81f6dab38319518046e4d2612d8ebb66
BeshoyHani <be4oy.hani@gmail.com>
2022-09-02 19:46:52 +0200

    port umber

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import "dotenv/config";
 import link_router from './routes/link.route.js';
 import user_router from './routes/user.route.js';
 import cors from 'cors';
-import  cookieParser  from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -11,13 +11,21 @@ const port = process.env.PORT || 3000;
 //     res.end("beshoy Hani");
 // });
 
-app.use(cors());
+app.use(cors({
+    "allowedHeaders": [
+        'Origin', 'X-Requested-With',
+        'Content-Type', 'Accept',
+        'X-Access-Token', 'Authorization', 'Access-Control-Allow-Origin',
+        'Access-Control-Allow-Headers',
+        'Access-Control-Allow-Methods'
+    ],
+    "methods": 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+    "preflightContinue": true,
+    "origin": '*',
+}));
+
 app.use(cookieParser());
 app.use(express.json());
-app.use((_req: Request, res: Response, next: NextFunction) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    next()
-})
 user_router(app);
 link_router(app);
 
