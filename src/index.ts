882a18bbe191bd0432b58f96ed29a3bde892ec28
BeshoyHani <be4oy.hani@gmail.com>
2022-09-06 01:16:23 +0200

    Deploy to heroku

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const port = process.env.PORT || 3000;
 // });
 
 app.use(function (_req: Request, res: Response, next: NextFunction) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+    res.setHeader('Access-Control-Allow-Origin', 'https://be-h.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
