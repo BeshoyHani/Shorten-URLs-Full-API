9ecf55875be67d6493f787b69c9c06cc0047c7eb
BeshoyHani <be4oy.hani@gmail.com>
2022-09-07 15:01:50 +0200

    Allow multi origins

--- src/index.ts
+++ src/index.ts
@@ -10,10 +10,14 @@ const port = process.env.PORT || 3000;
 //  app.use( (_req: Request, res: Response, next: NextFunction) => {
 //      console.log(_req.headers.host)
 //  });
+const allowedOrigins = ['https://be-h.vercel.app', 'https://be-h.netlify.app', 'http://localhost:4000'];
 
 app.use(function (_req: Request, res: Response, next: NextFunction) {
-    res.setHeader('Access-Control-Allow-Origin', 'https://be-h.vercel.app');
-    //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4000');
+    const origin = _req.headers.origin as string;
+    console.log(origin)
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
